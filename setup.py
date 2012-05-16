352a478185c1bcb609cd74f8488ec1c23a72a8b4
Grant Murphy <gmurphy@redhat.com>
2012-05-16 20:50:15 +1000

    more setup.py problems..

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     version='0.1.0', 
     author='Grant Murphy', 
     author_email='gmurphy@redhat.com', 
-    packages=find_packages(),
+    package_dir={'':'src'}
+    packages=find_packages('src'),
     url='https://github.com/gcmurphy/openshift',
     license="PSF",
     description='Create, control and configure OpenShift applications using Python', 
